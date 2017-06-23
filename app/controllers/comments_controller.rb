6ab0e41544c6f6a2599113a93211f34e44efe86a
michellejanosi <michelle.janosi@gmail.com>
2017-06-23 19:20:47 -0400

    Made some updates

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,23 @@
 class CommentsController < ApplicationController
   def index
+    @comments = Comment.all
   end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.invalid?
+      flash[:error] = "Why you leave comment blank? 😞 Try again."
+    end
+      redirect_to root_path
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:message)
+    end
 end
